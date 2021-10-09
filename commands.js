0732fddb9ae30fbaf214c28164fad514260e4488
Pukiediederik <diederik.boeren@gmail.com>
2021-10-09 16:17:06 +0200

    Refactored commands.js a bit

--- commands.js
+++ commands.js
@@ -7,39 +7,52 @@ exports.processCommands = (message, client) => {
 
     //split the message
     let fields = message.content.slice(3,message.content.length).split(' ');
-    if(!fields.length[0]) {
+    if(fields.length[0] == "") {
         botUtils.displayErrorMessage(message.channel, "No subcommand specified", "You seem to have not added a subcommand, use the help command to see what are valid options");
         return null;
     }
 
     if(fields[0] == "me"){
-        console.log("possibly disconnecting user");
-        message.member.voice.disconnect();
+        disconnectUser(message.channel, message.member, 0)
         return true;
-    } else if (fields[0] == "timer"){
+    } 
+    
+    else if (fields[0] == "timer"){
         if (fields.length < 2) {
             botUtils.displayErrorMessage(message.channel, "Not enough args", "Not enough arguments were given, make sure you added all the required arguments");
             return null;
         //check if a valid time has been given
         } else if (new RegExp(/^[0-9]+[m|s|h]$/i).test(fields[1])){
-            fields[1].toLowerCase();
-            //process time
-            let timeIdentifier = fields[1][fields[1].length-1];
-            let multiplier = 0;
-            switch(timeIdentifier){
-                case "s":
-                    multiplier = 1000;
-                    break;
-                case "m":
-                    multiplier = 60 * 1000;
-                    break;
-                case "h":
-                    multiplier = 60 * 60 * 1000;
-            }
-            let timeAmount = fields[1].slice(0,fields[1].length-1);
-            botUtils.displayInfoMessage(message.channel, `disconneting **you** in: **${timeAmount * multiplier / 1000}** seconds`);
-            //disconnect person
-            setTimeout(() => message.member.voice.disconnect(), timeAmount * multiplier);
+            disconnectUser(message.channel, message.member, fields[1]);
+            return true;
         }
     }
+}
+
+//function & helper functions to disconnect users
+function disconnectUser (channel, member, time){
+    //convert the time if necessary 
+    if (typeof(time) == "string") time = processTimeMsg(time);
+
+    //disconnect user
+    console.log("disconnecting user: " + member.displayName);
+    botUtils.displayInfoMessage(channel, `disconneting **${member.displayName}** in: **${time / 1000}** seconds`);
+    setTimeout(() => member.voice.disconnect(), time);
+}
+
+function processTimeMsg (msg){
+    msg.toLowerCase(); //make sure the message is in lowercase
+    let timeIdentifier = msg[msg.length-1];
+    switch(timeIdentifier){
+        case "s":
+            multiplier = 1000;
+            break;
+        case "m":
+            multiplier = 60 * 1000;
+            break;
+        case "h":
+            multiplier = 60 * 60 * 1000;
+    }
+    let timeAmount = msg.slice(0,msg.length-1);
+    return multiplier * timeAmount;
 }
